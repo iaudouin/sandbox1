7b9e69fda43d41c398099f2762bbae4644e4c917
iaudouin <121260376+iaudouin@users.noreply.github.com>
2022-12-22 15:23:08 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+//Hbs Prod
+var partner_id = '1919511';
+var uiconf_id = '35265851';
+//var idlist = [];
+
+//HBP
+//var partner_id = '506471';
+//var uiconf_id = '9952721';
+//var playlist_id = '';
+
+
+var embedIframeJs = 'https://cdnapisec.kaltura.com/p/'+partner_id+'/sp/'+partner_id+'00/embedIframeJs/uiconf_id/'+uiconf_id+'/partner_id/'+partner_id;
+var thumbpath = 'https://cdnapisec.kaltura.com/p/'+partner_id+'/thumbnail/entry_id/';
